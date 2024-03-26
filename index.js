5bf9ec8114171068895222578814831700e3d8af
Joy <kweyuayieko@gmail.com>
2024-03-27 00:13:13 +0300

    Finished looping lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, events) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+function countDown(startingNum) {
+  while (startingNum >= 0) {
+    console.log(startingNum);
+    startingNum--;
+  }
+}
+countDown(10)
